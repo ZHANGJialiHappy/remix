af427cfb178b0bf21fe4711caa902afbaad43dcc
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-11 20:13:48 +0200

    feat: navigate to notepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,14 @@
-import { Link } from "@remix-run/react";
+import { useNavigate } from "@remix-run/react";
 
 export default function Index() {
+  const navigate = useNavigate();
+
   return (
-    <main className="flex justify-center items-center text-center">
+    <main className="flex m-5 justify-center items-center text-center">
       <div>
-      <h1>A better way of keeping track of your notes!</h1>
-      <p>Try our early beta and never loose track of your notes again!</p>
-      <Link to="/notes"><button className="btn">Try now</button></Link>
+      <p className="text-2xl m-5">A better way of keeping track of your notes!</p>
+      <p>Try our early beta and never lose track of your notes again!</p>
+      <button className="btn btn-warning my-5" onClick={()=>{navigate("/notes")}}>Try now</button>
       </div>
     </main>
   );
