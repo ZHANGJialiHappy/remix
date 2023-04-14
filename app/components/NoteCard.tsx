00f9d8273ca8d8fec700b8832dda8e03754a3abc
Jiali Zhang <zhangjiali86@gmail.com>
2023-04-14 14:53:37 +0200

    replace bottom in card

--- app/components/NoteCard.tsx
+++ app/components/NoteCard.tsx
@@ -5,20 +5,20 @@ interface Props {
   note: NoteState
 }
 
-function NoteCard({note}:  Props) {
+function NoteCard({ note }: Props) {
   return (
-    <div>
-      <div className="card w-96 bg-base-100 shadow-xl image-full">
-  <figure><img src={daisy} alt="daisy" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{note.title}</h2>
-    <p>{note.content}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-outline btn-warning">see more</button>
-    </div>
-  </div>
-</div>
-    </div>
+      <div className="relative card w-96 bg-base-100 shadow-xl image-full">
+        <figure><img src={daisy} alt="daisy" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">{note.title}</h2>
+          <div className="overflow-y-hidden">
+          <p>{note.content}</p>
+          </div>
+          <div className="absolute card-actions justify-end right-8 bottom-8">
+            <button className="btn btn-outline btn-warning">see more</button>
+          </div>
+        </div>
+      </div>
   )
 }
 
